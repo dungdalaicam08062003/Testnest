c3d623866f1095658b3d77b6260f14f1c683f4b2
dungda <126653090+dungdalaicam08062003@users.noreply.github.com>
2026-04-08 17:50:09 +0700

    Update MainProgram.cs

--- MainProgram.cs
+++ MainProgram.cs
@@ -11,59 +11,46 @@ using WebsiteComputer.Models;
 
 
 
+
 var builder = WebApplication.CreateBuilder(args);
 
-// take server database
-var config = new ConfigurationBuilder()
-   .SetBasePath(Directory.GetCurrentDirectory())
-   .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-   .Build();
+var config = builder.Configuration;
 
+// JSON options
 builder.Services.ConfigureHttpJsonOptions(o =>
 {
     o.SerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-    o.SerializerOptions.WriteIndented = true;          // đẹp khi dev; prod thường để false
-    //o.SerializerOptions.Encoder = JavaScriptEncoder.Create(UnicodeRanges.All);
+    o.SerializerOptions.WriteIndented = true;
     o.SerializerOptions.Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
 });
 
-var connStr = config.GetConnectionString("Supabase")
-    ?? throw new InvalidOperationException("Missing ConnectionStrings:Supabase");
+// ✅ DB connection (local + render)
+var connStr =
+    config.GetConnectionString("Supabase") ??
+    Environment.GetEnvironmentVariable("DATABASE_CONNECTION") ??
+    throw new InvalidOperationException("Missing database connection");
 
-// //Read origin for CORs List
-// var allowedOrigins = config.GetSection("Cors:AllowedOrigins").Get<string[]>() ?? Array.Empty<string>();
-
-// builder.Services.AddCors(opt =>
-// {
-//     opt.AddPolicy("CorsPolicy", p =>
-//         p.WithOrigins(allowedOrigins)
-//          .AllowAnyHeader()
-//          .AllowAnyMethod());
-// });
+// ✅ CORS
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll",
-        policy => policy.AllowAnyOrigin()
-                        .AllowAnyMethod()
-                        .AllowAnyHeader());
+    options.AddPolicy("AllowAll", policy =>
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader());
 });
 
-// ✅ Controllers + Swagger
+// Controllers + Swagger
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
-
 var app = builder.Build();
-
-
 
 // ✅ Swagger UI
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.UseCors("CorsPolicy");
+app.UseCors("AllowAll");
 app.MapControllers();
 //http://localhost:5000/swagger
 
